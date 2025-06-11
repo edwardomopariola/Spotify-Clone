d0618e9139b0ec82ccba0b9ad998c8a17033e565
Edward Omopariola <femiedjr@gmail.com>
2025-06-11 17:48:19 -0400

    change backendurl

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const backendUrl = "https://spotify-clone-fcea296113be.herokuapp.com";
-// export const backendUrl = "http://localhost:5000";  // Uncomment this line to use the local backend during development
+// export const backendUrl = "https://spotify-clone-fcea296113be.herokuapp.com";
+export const backendUrl = "http://localhost:5000";  // Uncomment this line to use the local backend during development
